69227166b588f1dbb880368b8c8bbb761106e010
DJahzeel <darapc@lcg.unam.mx>
2024-06-06 01:53:07 -0600

    Huum nos equivocamos
    nos falto una carpeta unu

--- scripts/main.py
+++ scripts/main.py
@@ -1,4 +1,4 @@
-from dna_analysis.operations.translation import translate_all_frames
+from ProyectoPython.operations.translation import translate_all_frames
 
 if __name__ == "__main__":
     archivo_entrada = input("Ingresa la ruta al archivo FASTA de entrada: ")
